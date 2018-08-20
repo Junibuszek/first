33afc58ec8f84b43a0cb0b0b4a3f901684f25602
Junibuszek <42551250+Junibuszek@users.noreply.github.com>
2018-08-20 20:05:57 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+print("Mój pierwsyz kalkulator :P")
+while True:
+    print("pierwsza liczba")
+    x = input()
+    print('działanie które chcesz wykonać (x , / , - , + )')
+    z = input()
+    print('druga liczba')
+    y = input()
+    x = int(x)
+    y = int(y)
+
+    w = float()
+    if z == "x":
+        w = int(x*y)
+    if z == "+":
+        w = int(x+y)
+    if z == "-":
+        w = int(x-y)
+    if z == "/":
+        w = (x/y)
+    print(x, z, y, "=", w)
